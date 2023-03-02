99220ed81731320abb12b094d7385310e49c0277
Nathan.P <nathan.poncet@crea-inseec.com>
2023-03-02 11:56:28 +0100

    :zap: improve form

--- app/src/features/datas/addData/index.tsx
+++ app/src/features/datas/addData/index.tsx
@@ -1,26 +1,57 @@
-import { DatePicker } from '@mui/lab';
 import {
   Button,
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
   FormControl,
   InputLabel,
+  MenuItem,
   Select,
-  TextField,
 } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Dayjs } from 'dayjs';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers';
 import axios from '@/libs/axios';
+import { Controller, useForm } from 'react-hook-form';
 
 export const AddData = () => {
-  const [dataType, setDataType] = useState();
+  const [categories, setCategories] = useState<{
+    '@context': string;
+    '@id': String;
+    '@type': 'hydra:Collection';
+    'hydra:member': {
+      '@id': string;
+      '@type': string;
+      category: String;
+      id: number;
+      unit: String;
+      name: String;
+    }[];
+    'hydra:totalItems': number;
+  }>();
+  const [dataTypes, setDataTypes] = useState<{
+    '@context': string;
+    '@id': String;
+    '@type': 'hydra:Collection';
+    'hydra:member': {
+      '@id': string;
+      '@type': string;
+      category: String;
+      id: number;
+      unit: String;
+      name: String;
+    }[];
+    'hydra:totalItems': number;
+  }>();
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState<Dayjs | null>(null);
+  const { control, handleSubmit } = useForm<{
+    category: number;
+    date: string;
+    value: number;
+    dataType: number;
+    user: number;
+  }>();
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -32,32 +63,97 @@ export const AddData = () => {
 
   useEffect(() => {
     axios
+      .get('/categories')
+      .then(({ data }) => {
+        setCategories(data);
+      })
+      .catch((err) => handleClose());
+    axios
       .get('/data_types')
-      .then(({ data }) => setDataType(data))
+      .then(({ data }) => {
+        console.log(data);
+        
+        setDataTypes(data);
+      })
       .catch((err) => handleClose());
   }, []);
 
+  const onSubmit = ({}: {
+    date: string;
+    value: number;
+    dataType: number;
+    user: number;
+  }) => {
+    return new Promise((resolve, reject) => {
+      console.log(value);
+    });
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Button variant="outlined" onClick={handleClickOpen}>
         Ajouter une donnée
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Ajouter une donnée</DialogTitle>
         <DialogContent>
+          <br />
           <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={age}
-              label="Age"
-              onChange={handleChange}
-            >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
+            <InputLabel id="category-select-label">Category</InputLabel>
+            <Controller
+              name="category"
+              control={control}
+              defaultValue={categories?.['hydra:member'][0].id}
+              render={({ field: { onChange, onBlur, value, name, ref } }) => (
+                <Select
+                  labelId="category-select-label"
+                  id="category-select"
+                  label="Category"
+                  ref={ref}
+                  value={
+                    categories?.['hydra:member'].find(
+                      (category) => category.id === value
+                    )?.id
+                  }
+                  onChange={(data) => onChange(data.target.value)}
+                >
+                  {categories?.['hydra:member'].map((category) => (
+                    <MenuItem key={category.id} value={category.id}>
+                      {category.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              )}
+            />
+          </FormControl>
+          <br />
+          <FormControl fullWidth>
+            <InputLabel id="category-select-label">Category</InputLabel>
+            <Controller
+              name="dataType"
+              control={control}
+              defaultValue={dataTypes?.['hydra:member'][0].id}
+              render={({ field: { onChange, onBlur, value, name, ref } }) => (
+                <Select
+                  labelId="category-select-label"
+                  id="category-select"
+                  label="Category"
+                  ref={ref}
+                  value={
+                    categories?.['hydra:member'].find(
+                      (category) => category.id === value
+                    )?.id
+                  }
+                  onChange={(data) => onChange(data.target.value)}
+                >
+                  {categories?.['hydra:member'].map((category) => (
+                    <MenuItem key={category.id} value={category.id}>
+                      {category.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              )}
+            />
           </FormControl>
         </DialogContent>
         <DialogActions>
@@ -65,6 +161,6 @@ export const AddData = () => {
           <Button onClick={handleClose}>Ajouter</Button>
         </DialogActions>
       </Dialog>
-    </>
+    </form>
   );
 };
